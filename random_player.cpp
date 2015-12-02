6d59b859d914847d6ce0be3d4a2975577234ed61
Eitan Frachtenberg <eitan@specexec.com>
2015-12-02 12:28:39 -0800

    Implemented dummy random player

---
+++ random_player.cpp
@@ -0,0 +1,25 @@
+//
+// Created by eitan on 12/2/15.
+//
+
+#include "random_player.h"
+
+#include <cstdlib>
+#include <random>
+
+namespace grandeur {
+
+GameMove
+RandomPlayer::getMove(Board&, Cards&, const Moves& legal)
+{
+    static const char* seedstr = std::getenv("GRANDEUR_PLAYER_SEED");
+    static std::mt19937_64 generator(seedstr? std::atoll(seedstr) : 1ULL);
+
+    std::uniform_int_distribution<> dist(0, legal.size() - 1);
+    return legal.at(dist(generator));
+}
+
+static PlayerFactory::Registrator registrator("random",
+                             [](player_id_t pid){ return new RandomPlayer(pid); });
+
+} // namespace
---
+++ random_player.h
@@ -0,0 +1,19 @@
+// An implementation of a simple "AI" player that picks any legal move randomly.
+// Note: you can set the see for the PRNG with the environment variable GRANDEUR_PLAYER_SEED
+//
+// Created by eitan on 12/2/15.
+//
+
+#pragma once
+
+#include "player.h"
+
+namespace grandeur {
+
+class RandomPlayer final : public Player {
+  public:
+    RandomPlayer(player_id_t pid) : Player(pid) {}
+    virtual GameMove getMove(Board& board, Cards& hidden, const Moves& legal);
+};
+
+}  // namespace
