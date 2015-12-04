9d76c7c04009fb0b0f2c7f27a5f4f7eb50165c2c
Eitan Frachtenberg <eitan@specexec.com>
2015-12-04 13:59:49 -0800

    Add a script to run two-way tournaments

---
+++ arena.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#
+# This script pits players against each other multiple times in the grandeur
+# game, and collects statistics and logs about the games. It requires two
+# command line arguments, the number of iterations and the name of a directory
+# to keep log files in, and two player names to pass to grandeur.
+#
+# Written by Eitan Frachtenberg, 2015-12-03.
+#
+import os, re, subprocess, sys
+
+bin = "./grandeur"
+
+###################################
+def logfn(logdir, iter):
+    return logdir + "/game." + str(iter)
+
+
+###################################
+def run_game(iter, logdir, p0, p1):
+    cmdline = bin + " -s " + str(iter) + " -l " + logfn(logdir, iter) + " " + p0 + " " + p1
+    subprocess.call(cmdline, shell=True)
+
+###################################
+def get_winner(fn):
+    last = subprocess.check_output("tail -1 " + fn, shell=True)
+    m = re.match(r'GAME OVER! Player (\d) wins!', last)
+    if m is None:
+        m = re.match(r'GAME OVER! Stalemate!', last)
+        if m is None:
+            print("Couldn't find winner in log file: " + fn)
+            sys.exit(-2)
+        else:
+            return "Tie"
+
+    return "P" + m.group(1)
+
+
+###################################
+###### Main
+if len(sys.argv) != 5:
+    print("Need four parameters: no. of iterations, directory, and two players")
+    sys.exit(-1)
+
+niter = int(sys.argv[1])
+logdir = sys.argv[2]
+p0 = sys.argv[3]
+p1 = sys.argv[4]
+wins = { "P0": 0, "P1": 0, "Tie": 0 }
+
+os.mkdir(logdir)
+
+for i in range(1, niter + 1):
+    run_game(i, logdir, p0, p1)
+    winner = get_winner(logfn(logdir, i))
+    wins[winner] = wins[winner] + 1
+
+print(wins)
